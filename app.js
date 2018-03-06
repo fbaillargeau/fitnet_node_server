f9b636fda90c324eeac436c80d1df46f5acd2866
Unknown <fbaillargeau@palo-it.com>
2018-03-06 17:38:43 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,127 @@
+const express = require('express')
+const app = express()
+var request = require("request")
+var server = require('http').createServer(app);
+var redis = require('redis');
+var _ = require('lodash');
+var log4js = require('log4js');
+
+//Création du logger
+var logger = log4js.getLogger();
+logger.level = 'debug';
+
+//Création du client redis
+var redis_host = process.env.REDIS_HOST !== undefined ? process.env.REDIS_HOST : '192.168.99.100';
+var redis_port = process.env.REDIS_PORT !== undefined ? process.env.REDIS_PORT : 6379;
+var redisClient = redis.createClient(redis_port, redis_host);
+
+//Headers pour les requêtes http
+var headersOneSignal = {
+    "Content-Type": "application/json",
+    "Authorization": "Basic OWQ5ZjY1YWQtYWM3NC00YWViLWE2ZmItNGQ2MzQxY2RhMmVi"
+};
+
+var headersFitnet = {
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "Basic ZmJhaWxsYXJnZWF1QHBhbG8taXQuY29tOnZoVDczOSFIRmU="
+}
+
+//Options pour les requêtes http
+var optionOneSignal = {
+    host: "onesignal.com",
+    port: 443,
+    path: "/api/v1/notifications",
+    method: "POST",
+    headers: headersOneSignal
+};
+
+var optionFitnet = {
+    host: "evaluation.fitnetmanager.com",
+    port: 443,
+    path: "/FitnetManager/rest/contracts/1",
+    method: "GET",
+    headers: headersFitnet
+}
+
+//Permet d'envoyer des notifications
+var sendNotification = function(data, nbMission) {
+
+    var https = require('https');
+    var req = https.request(optionOneSignal, function(res) {
+        res.on('data', function(data) {
+            logger.info("Notification envoyée");
+            redisClient.set('nbMissions', nbMission++);
+        });
+    });
+
+    req.on('error', function(e) {
+        logger.error(e)
+    });
+
+    req.write(JSON.stringify(data));
+    req.end();
+};
+
+//Permet de récupérer les missions
+var getMissions = function() {
+    var https = require('https');
+    var req = https.request(optionFitnet, function(res) {
+        var content = '';
+        res.on('data', function(data) {
+            content += data;
+        }).on('end', function() {
+            //Récupération des missions + tri des missions par lotId (ordre de création décroissante)
+            var missions = _.orderBy(JSON.parse(content), ['lotId'], ['desc']);
+            var nbMissions = missions.length;
+
+            //Vérification de l'existance de la clé nbMissions
+            redisClient.exists('nbMissions', function(err, reply) {
+                if (reply === 1) {
+                    //La clé existe
+                    redisClient.get('nbMissions', function(err, reply) {
+                        if (reply !== nbMissions) {
+                            var diff = nbMissions - reply;
+                            for (var i = 0; i < diff; i++) {
+                                var mission = missions[i];
+                                var contentMessage = `Nouvelle mission : ${mission.title} du ${mission.beginDate} au ${mission.endDate} \nDescription:\n${mission.description} !`;
+                                var message = createMessage(contentMessage);
+                                sendNotification(message, nbMissions);
+                            }
+                        }
+                    });
+                } else {
+                    //La clé n'existe pas. Initialisation du compteur de missions
+                    redisClient.set('nbMissions', missions.length)
+                }
+            });
+        });
+    });
+
+    req.on('error', function(e) {
+        logger.error(e);
+    });
+
+    req.end();
+};
+
+//Permet de créer un message pour les notifications
+var createMessage = function(message) {
+    return {
+        app_id: "347ad83a-ffad-4502-b61c-dd47b7963dee",
+        contents: { "en": message },
+        included_segments: ["All"]
+    }
+}
+
+//Lancement du serveur node sur le port 3123
+server.listen(3123, function() {
+    logger.info('Lancement de l\'application sur le port 3123')
+        //FOR TEST
+    redisClient.get('nbMissions', function(err, reply) {
+        redisClient.set('nbMissions', reply - 1);
+    });
+
+    setInterval(function() {
+        getMissions();
+    }, 20000)
+});
